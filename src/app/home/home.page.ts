cee22dfd1301e3d1e6186c3eba5338394408efd9
Didar Burmaganov <deedarb@gmail.com>
2024-09-26 17:25:11 +0500

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonRadioGroup, IonRadio } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonRadioGroup, IonRadio, FormsModule],
 })
 export class HomePage {
+  // <!-- custom code begins... -->
+  selected: string | undefined = 'strawberries';
+  plainSelected: number | undefined = 1;
+  // <!-- custom code ends... -->
+
   constructor() {}
 }
